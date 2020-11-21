bbfb119b05230442fcb07bf35b1f0c0e8b92df56
cofa <cofa@MacBook-Pro.local>
2020-11-21 15:58:11 +0300

    Добавил метод способ нахождения пресечения списка списков списков ))

---
+++ intersection.py
@@ -0,0 +1,14 @@
+def intersection(L):
+    L_sets = []
+    for l in L: #  [ [[], []], [[], []] ]  ---->  [ {(), ()}, {(), ()} ],  где {} это set "множество"
+        l_tuples = [tuple(x) for x in l]  # Списки в списке привожу к таплам
+        L_sets.append(set(tuple(l_tuples)))
+    print(L_sets)
+    print(L_sets[0].intersection(*L_sets))
+
+if __name__ == "__main__":
+    L1 = [[0, 1], [2,6,9], [3,5], [4,8], [7]]
+    L2 = [[0, 1], [2, 6, 9], [3, 4, 7], [5, 8]]
+    L3 = [[2, 6, 9]]
+    L = [L1, L2, L3]
+    intersection(L)
