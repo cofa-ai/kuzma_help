dee39dc7e938926fad5fa243104ec0a25c61e14f
cofa <cofa@MacBook-Pro.local>
2020-11-21 16:02:30 +0300

    return value in intersection

--- intersection.py
+++ intersection.py
@@ -3,12 +3,11 @@ def intersection(L):
     for l in L: #  [ [[], []], [[], []] ]  ---->  [ {(), ()}, {(), ()} ],  где {} это set "множество"
         l_tuples = [tuple(x) for x in l]  # Списки в списке привожу к таплам
         L_sets.append(set(tuple(l_tuples)))
-    print(L_sets)
-    print(L_sets[0].intersection(*L_sets))
+    return set.intersection(*L_sets)
 
 if __name__ == "__main__":
-    L1 = [[0, 1], [2,6,9], [3,5], [4,8], [7]]
+    L1 = [[0, 1], [2, 6, 9], [3, 5], [4,8], [7]]
     L2 = [[0, 1], [2, 6, 9], [3, 4, 7], [5, 8]]
     L3 = [[2, 6, 9]]
     L = [L1, L2, L3]
-    intersection(L)
+    print(intersection(L))
